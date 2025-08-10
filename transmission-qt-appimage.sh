0b3256e8263efdbe91fa7cc33cf121273e32e7fb
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-10 00:14:55 -0400

    simplify deployment with wrapper scripts

--- transmission-qt-appimage.sh
+++ transmission-qt-appimage.sh
@@ -2,11 +2,10 @@
 
 set -ex
 
-export ARCH="$(uname -m)"
-URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
-URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
+ARCH="$(uname -m)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 PELF="https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH"
 
 # The arch package does not have icon lol
@@ -15,64 +14,40 @@ ICON="https://raw.githubusercontent.com/transmission/transmission/refs/heads/mai
 VERSION="$(pacman -Q transmission-qt | awk '{print $2; exit}')"
 echo "$VERSION" > ~/version
 
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME=Transmission-Qt-"$VERSION"-anylinux-"$ARCH".AppImage
+
 # Prepare AppDir
-mkdir -p ./AppDir && (
-	cd ./AppDir
-	cp -rv /usr/share/applications/transmission-qt.desktop ./
-	wget --retry-connrefused --tries=30 "$ICON" -O ./transmission.svg
-	cp -v ./transmission.svg ./.DirIcon
+mkdir -p ./AppDir
+cp -v /usr/share/applications/transmission-qt.desktop ./AppDir
+wget --retry-connrefused --tries=30 "$ICON" -O ./AppDir/transmission.svg
+cp -v ./AppDir/transmission.svg ./AppDir/.DirIcon
 
-	# ADD LIBRARIES
-	wget --retry-connrefused --tries=30 "$SHARUN" -O ./sharun-aio
-	chmod +x ./sharun-aio
-	xvfb-run -a \
-		./sharun-aio l -p -v -e -s -k                \
-		/usr/bin/transmission-qt                     \
-		/usr/lib/qt6/plugins/imageformats/*          \
-		/usr/lib/qt6/plugins/iconengines/*           \
-		/usr/lib/qt6/plugins/platforms/*             \
-		/usr/lib/qt6/plugins/platformthemes/*        \
-		/usr/lib/qt6/plugins/platforminputcontexts/* \
-		/usr/lib/qt6/plugins/styles/*                \
-		/usr/lib/qt6/plugins/tls/*                   \
-		/usr/lib/qt6/plugins/xcbglintegrations/*     \
-		/usr/lib/qt6/plugins/wayland-*/*
-	rm -f ./sharun-aio
-	ln ./sharun ./AppRun
-	./sharun -g
-)
+# ADD LIBRARIES
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/transmission-qt /usr/lib/qt6/plugins/tls/*
+ln ./AppDir/sharun ./AppDir/AppRun
 
-# MAKE APPIAMGE WITH URUNTIME
-wget --retry-connrefused --tries=30 "$URUNTIME"      -O ./uruntime
-wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O ./uruntime-lite
-chmod +x ./uruntime*
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
 
-# Add udpate info to runtime
-echo "Adding update information \"$UPINFO\" to runtime..."
-./uruntime-lite --appimage-addupdinfo "$UPINFO"
-
-echo "Generating AppImage..."
-./uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0          \
-	--no-history --no-create-timestamp   \
-	--compression zstd:level=22 -S26 -B8 \
-	--header uruntime-lite               \
-	-i ./AppDir -o ./Transmission-Qt-"$VERSION"-anylinux-"$ARCH".AppImage
-
-wget -O ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH" 
+# make appbundle
+wget ---retry-connrefused --tries=30 "$PELF" -O ./pelf
 chmod +x ./pelf
-
 echo "Generating [dwfs]AppBundle..."
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.dwfs.AppBundle.zsync"
-./pelf --add-appdir ./AppDir \
+./pelf \
+	--compression "-C zstd:level=22 -S26 -B8" \
 	--appimage-compat                         \
 	--add-updinfo "$UPINFO"                   \
 	--appbundle-id="com.transmissionbt.Transmission#github.com/$GITHUB_REPOSITORY:$VERSION@$(date +%d_%m_%Y)" \
-	--compression "-C zstd:level=22 -S26 -B8" \
+	--add-appdir ./AppDir                     \
 	--output-to Transmission-Qt-"$VERSION"-anylinux-"$ARCH".dwfs.AppBundle
 
 echo "Generating zsync file..."
-zsyncmake *.AppImage -u *.AppImage
 zsyncmake *.AppBundle -u *.AppBundle
 
 echo "All Done!"
