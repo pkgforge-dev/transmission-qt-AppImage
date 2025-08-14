38804344c73d54ab99385f5b2f75f76787aeae0c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-13 21:25:25 -0400

    use more wrapper scripts, simplify deployment.

--- transmission-qt-appimage.sh
+++ transmission-qt-appimage.sh
@@ -1,27 +1,22 @@
 #!/bin/sh
 
-set -ex
+set -eux
 
 ARCH="$(uname -m)"
 URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-PELF="https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH"
-
-# The arch package does not have icon lol
-ICON="https://raw.githubusercontent.com/transmission/transmission/refs/heads/main/icons/hicolor_apps_scalable_transmission.svg"
 
 VERSION="$(pacman -Q transmission-qt | awk '{print $2; exit}')"
 echo "$VERSION" > ~/version
 
+export ADD_HOOKS="self-updater.bg.hook"
+# The arch package does not have icon lol
+export ICON="https://raw.githubusercontent.com/transmission/transmission/refs/heads/main/icons/hicolor_apps_scalable_transmission.svg"
+export DESKTOP="/usr/share/applications/transmission-qt.desktop"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export OUTNAME=Transmission-Qt-"$VERSION"-anylinux-"$ARCH".AppImage
 
-# Prepare AppDir
-mkdir -p ./AppDir
-cp -v /usr/share/applications/transmission-qt.desktop ./AppDir
-wget --retry-connrefused --tries=30 "$ICON" -O ./AppDir/transmission.svg
-
-# ADD LIBRARIES
+# Deploy dependencies
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun /usr/bin/transmission-qt /usr/lib/qt6/plugins/tls/*
@@ -32,10 +27,11 @@ chmod +x ./uruntime2appimage
 ./uruntime2appimage
 
 # make appbundle
-wget --retry-connrefused --tries=30 "$PELF" -O ./pelf
+wget --retry-connrefused --tries=30 \
+	"https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH" -O ./pelf
 chmod +x ./pelf
 echo "Generating [dwfs]AppBundle..."
-UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.dwfs.AppBundle.zsync"
+UPINFO="$(echo "$UPINFO" | sed 's#.AppImage.zsync#*.AppBundle.zsync#g')"
 ./pelf \
 	--compression "-C zstd:level=22 -S26 -B8" \
 	--appimage-compat                         \
