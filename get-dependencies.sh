5086697e9744ac42fad520a530310fbfb464485f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-14 02:58:07 -0400

    simplify deps install

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 
-set -ex
+set -eux
 
-sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
-ARCH="$(uname -m)"
-
-if [ "$ARCH" = 'x86_64' ]; then
-	PKG_TYPE='x86_64.pkg.tar.zst'
-else
-	PKG_TYPE='aarch64.pkg.tar.xz'
-fi
-
-QT6_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/qt6-base-iculess-$PKG_TYPE"
-LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
-OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
 echo "Installing dependencies..."
 echo "---------------------------------------------------------------"
@@ -40,14 +29,10 @@ pacman -Syu --noconfirm \
 	xorg-server-xvfb       \
 	zsync
 
-echo "Installing debloated pckages..."
+echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$QT6_URL"    -O  ./qt6-base.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$LIBXML_URL" -O  ./libxml2.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$OPUS_URL"   -O  ./opus.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh qt6-base-mini libxml2-mini opus-mini gdk-pixbuf2-mini
 
-pacman -U --noconfirm ./*.pkg.tar.zst
-rm -f ./*.pkg.tar.zst
-
-echo "All done!"
-echo "---------------------------------------------------------------"
+pacman -Q transmission-qt | awk '{print $2; exit}' > ~/version
