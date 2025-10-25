9bc7d3fa696187d293897b579d7262fd9cff3f0b
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-24 20:43:28 -0400

    use common packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,32 +7,30 @@ EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImage
 echo "Installing dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	alsa-lib               \
-	base-devel             \
-	cmake                  \
-	curl                   \
-	git                    \
-	gtk3                   \
-	hicolor-icon-theme     \
-	libglvnd               \
-	libpulse               \
-	libx11                 \
-	libxrandr              \
-	libxss                 \
-	pipewire-audio         \
-	pulseaudio             \
-	pulseaudio-alsa        \
-	qt6ct                  \
-	qt6-wayland            \
-	transmission-qt        \
-	wget                   \
-	xorg-server-xvfb       \
+	alsa-lib         \
+	base-devel       \
+	cmake            \
+	curl             \
+	git              \
+	libglvnd         \
+	libpulse         \
+	libx11           \
+	libxrandr        \
+	libxss           \
+	pipewire-audio   \
+	pulseaudio       \
+	pulseaudio-alsa  \
+	qt6ct            \
+	qt6-wayland      \
+	transmission-qt  \
+	wget             \
+	xorg-server-xvfb \
 	zsync
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh qt6-base-mini libxml2-mini opus-mini gdk-pixbuf2-mini
+./get-debloated-pkgs.sh --add-common --prefer-nano
 
 pacman -Q transmission-qt | awk '{print $2; exit}' > ~/version
